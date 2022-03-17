95118dc67fdd4970ed647fac1247ecbf806a6cc1
phaa <pedrohamorim2011@hotmail.com>
2022-03-17 11:57:45 -0300

    Teste

--- arduino/asyncServer/asyncServer.ino
+++ arduino/asyncServer/asyncServer.ino
@@ -3,11 +3,12 @@
 #include "ESPAsyncWebServer.h"
 #include "AsyncJson.h"
 #include "ArduinoJson.h"
-#include <Preferences.h>
+#include <EEPROM.h>
 #include <LiquidCrystal_I2C.h>
 
 #define PUMP_RELAY_PIN 5
 #define EXAUST_RELAY_PIN 18
+#define SECTIONS_COUNT 6
 
 // Debug
 #define DEBUG
@@ -19,37 +20,38 @@
   #define DEBUG_PRINTLN(x)
 #endif
 
+// Web
 const char* ssid = "Tupi";
 const char* password =  "12345678";
-
 AsyncWebServer server(80);
-Preferences preferences;
+
+// LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-short lcdState = 0;
+byte lcdState = 0;
 
 // Estado da irrigação
-byte pumpState;
-byte exaustingState;
-byte fertirrigationState;
-bool automaticIrrigation;
-bool automaticExausting;
-bool automaticFertirrigation;
+//byte pumpState;
+//byte exaustingState;
+//byte fertirrigationState;
+bool automaticIrrigation = false;
+bool automaticExausting = false;
+bool automaticFertirrigation = false;
 
 // Controle de seções de irrigação ativas
-byte sections[] = { LOW, LOW, LOW };
+//byte sections[SECTIONS_COUNT];
 
 // Contador de tempo
 unsigned long counter1s = 0;
 unsigned long counter2s = 0;
 
-void setup() {
+void setup() 
+{
+  // Se debug estiver on, ativa serial
   #ifdef DEBUG
     Serial.begin(115200);
   #endif
   
-  // Configuração dos relés
-  pinMode(PUMP_RELAY_PIN, OUTPUT);
-  pinMode(EXAUST_RELAY_PIN, OUTPUT);
+  setupPins();
 
   // Buscar dados da armazenagem
   initPreferences();
@@ -60,7 +62,6 @@ void setup() {
   lcdPrint(0, 0, "Tupi Agro");
   lcdPrint(0, 1, "Inicializando...");
 
-  // WiFi
   initWifi();
 
   initServer();
@@ -69,67 +70,80 @@ void setup() {
   lcdPrint(0, 1, "Sistema Online");
 }
 
-void loop() {
-  if ( millis() - counter1s >= 1000 ) {
+void loop() 
+{
+  if ( millis() - counter1s >= 1000 ) 
+  {
     digitalWrite(PUMP_RELAY_PIN, pumpState);
     digitalWrite(EXAUST_RELAY_PIN, exaustingState);
     counter1s = millis();
     // Seta o estado da bomba de acordo com a disponibilidade no arrayde sections
   }
   
-  if ( millis() - counter2s >= 3000 ) {
+  if ( millis() - counter2s >= 3000 ) 
+  {
     refreshLCD();
     counter2s = millis();
   }
 } 
 
-void initPreferences() {
-  preferences.begin("bomba", false);
-  /*
-   * pumpState = preferences.getInt("ps", LOW);
-   * exaustingState = preferences.getInt("es", LOW);
-   * fertirrigationState = preferences.getInt("fs", LOW);
-   */
-  automaticIrrigation = preferences.getBool("ai", false);
-  automaticExausting = preferences.getBool("ae", false);
-  automaticFertirrigation = preferences.getBool("af", false);
-
-  DEBUG_PRINTLN("Ler sections");
-  // Recupera as seções que estavam ligadas e desligadas
-  for (int i = 0; i < sizeof sections; i++) {
-    String sectionName = "s" + String(i);
-    sections[i] = preferences.getInt(sectionName.c_str(), LOW);
-    DEBUG_PRINT(sectionName+" ");
-    DEBUG_PRINTLN(sections[i]);
-   //Aproveita o loop e já seta as solenoides de acordo com a secao
-  }
-
-  preferences.end();
+void setupPins() 
+{
+  // Configuração dos relés
+  pinMode(PUMP_RELAY_PIN, OUTPUT);
+  pinMode(EXAUST_RELAY_PIN, OUTPUT);
 }
 
-void initServer() {
+void initPreferences() 
+{
+  EEPROM.begin(SECTIONS_COUNT);
+  
+  DEBUG_PRINTLN("Lendo seções na memória...");
+  
+  for (int addressIndex = 0; addressIndex < (SECTIONS_COUNT - 1); addressIndex++) 
+  {
+    //sections[addressIndex] = EEPROM.read(addressIndex);
+    DEBUG_PRINT("Seção: ");
+    DEBUG_PRINT(addressIndex);
+    DEBUG_PRINT(" Valor: ");
+    DEBUG_PRINTLN(EEPROM.read(addressIndex));
+    //DEBUG_PRINTLN(sections[addressIndex]);
+  }
+}
+
+void initServer() 
+{
   // Configuração das rotas do servidor
-  server.on("/check", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/check", HTTP_GET, [](AsyncWebServerRequest * request) 
+  {
     DEBUG_PRINT(request->client()->remoteIP());
     DEBUG_PRINTLN(" solicitou uma checagem.");
-    request->send(200, "text/plain", "ok");
+    request->send(200, "text/plain", "Módulo ESP32 ok!");
+  });
+
+  server.on("/restart", HTTP_GET, [](AsyncWebServerRequest * request) 
+  {
+    request->send(200, "text/plain", "Reiniciando módulo...");
+    ESP.restart();
   });
   
-  server.on("/irrigationstatus", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/irrigationstatus", HTTP_GET, [](AsyncWebServerRequest * request) 
+  {
     AsyncResponseStream *response = request->beginResponseStream("application/json");
     
     DynamicJsonDocument doc(1024);
     JsonObject greenhouseStats = doc.to<JsonObject>();
-    greenhouseStats["pumpState"] = pumpState;
-    greenhouseStats["automaticIrrigation"] = automaticIrrigation;
-    greenhouseStats["exaustingState"] = exaustingState;
+    //greenhouseStats["pumpState"] = pumpState;
+    //greenhouseStats["automaticIrrigation"] = automaticIrrigation;
+    //greenhouseStats["exaustingState"] = exaustingState;
     greenhouseStats["automaticExausting"] = automaticExausting;
     greenhouseStats["fertirrigationState"] = fertirrigationState;
     greenhouseStats["automaticFertirrigation"] = automaticFertirrigation;
 
     JsonArray sectionsJSON = greenhouseStats.createNestedArray("sections");
-    for (int i = 0; i < sizeof sections; i++) {
-      sectionsJSON.add(sections[i]);
+    for (int addressIndex = 0; addressIndex < (SECTIONS_COUNT - 1); addressIndex++) 
+    {
+      sectionsJSON.add(EEPROM.read(addressIndex));
     }
     
     serializeJson(greenhouseStats, *response);
@@ -138,11 +152,13 @@ void initServer() {
     request->send(response);
   });
 
-  server.on("/command", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/command", HTTP_GET, [](AsyncWebServerRequest * request) 
+  {
     //AsyncResponseStream *response = request->beginResponseStream("application/json");
     preferences.begin("bomba", false);
     
-    if (request->hasParam("automaticIrrigation")) {
+    if (request->hasParam("automaticIrrigation")) 
+    {
       String iaParam = request->getParam("automaticIrrigation")->value();
       automaticIrrigation = (iaParam.toInt()) ? true : false;
       preferences.putBool("ai", automaticIrrigation);
@@ -151,7 +167,8 @@ void initServer() {
       DEBUG_PRINTLN(automaticIrrigation);
     }
 
-    if (request->hasParam("automaticExausting")) {
+    if (request->hasParam("automaticExausting")) 
+    {
       String iaParam = request->getParam("automaticExausting")->value();
       automaticExausting = (iaParam.toInt()) ? true : false;
       preferences.putBool("ae", automaticExausting);
@@ -160,7 +177,8 @@ void initServer() {
       DEBUG_PRINTLN(automaticExausting);
     }
 
-    if (request->hasParam("automaticFertirrigation")) {
+    if (request->hasParam("automaticFertirrigation")) 
+    {
       String iaParam = request->getParam("automaticFertirrigation")->value();
       automaticFertirrigation = (iaParam.toInt()) ? true : false;
       preferences.putBool("af", automaticFertirrigation);
@@ -196,33 +214,28 @@ void initServer() {
     request->send(200, "text/plain", "ok");
   });
 
-  server.on("/togglesection", HTTP_GET, [](AsyncWebServerRequest * request) {
-    // Se o automático estiver desligado e tiver o parâmetro da bomba
-    if (request->hasParam("pump") && request->hasParam("section")) {
-      preferences.begin("bomba", false);
-      
-      String pumpParam = request->getParam("pump")->value();
-      pumpState = (pumpParam.toInt()) ? HIGH : LOW;
-      preferences.putInt("ps", pumpState);
-      
+  server.on("/togglesection", HTTP_GET, [](AsyncWebServerRequest * request) 
+  {
+    if (request->hasParam("pump") && request->hasParam("section")) 
+    {
       int sectionParam = request->getParam("section")->value().toInt();
-      sections[sectionParam] = pumpState;
-     
-      String section = "s" + String(sectionParam);
-      preferences.putInt(section.c_str(), pumpState);
+      int pumpParam = request->getParam("pump")->value().toInt();
+
+      // Salva na memória EEPROM
+      EEPROM.write(sectionParam, pumpParam);
+      EEPROM.commit();
       
       DEBUG_PRINT("Seção (id, valor) ");
-      DEBUG_PRINT(sectionParam);
+      DEBUG_PRINT(char_array);
       DEBUG_PRINT(" ");
-      DEBUG_PRINTLN(sections[sectionParam]);
-      
-      DEBUG_PRINT("Bomba: ");
-      DEBUG_PRINTLN(pumpState);
-      preferences.end();
+      DEBUG_PRINTLN(pumpParam);
+      //togglePump();
     }
 
     DEBUG_PRINT(request->client()->remoteIP());
-    DEBUG_PRINTLN(" solicitou mudar uma seção.");
+    DEBUG_PRINT(" solicitou mudança na seção ");
+    DEBUG_PRINTLN(sectionParam);
+    request->send(200, "text/plain", "ok");
   });
 
   // Autoriza o CORS
@@ -230,15 +243,30 @@ void initServer() {
   server.begin();
 }
 
-void toggleSection() {
+void togglePump() 
+{
+  int pump = 0;
   
+  for (int i = 0; i < sizeof sections/sizeof sections[0]; i++) 
+  {
+    pump = pump || sections[i];
+  }
+
+  DEBUG_PRINT("Valor da bomba: ");
+  DEBUG_PRINTLN(pump);
+  
+  preferences.begin("bomba", false);
+  preferences.putInt("ps", pump);
+  preferences.end();
 }
 
-void initWifi() {
-  WiFi.begin(ssid, password);
+void initWifi() 
+{
   DEBUG_PRINTLN("Conectando ao WiFi..");
   lcdPrint(0, 1, "Conectando WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(1000);
     DEBUG_PRINT(".");
   }
@@ -247,29 +275,29 @@ void initWifi() {
   DEBUG_PRINTLN(WiFi.macAddress());
 }
 
-void lcdPrint(short cursor, short line, String msg) {
+void lcdPrint(short cursor, short line, String msg) 
+{
   lcd.setCursor(cursor, line);
   lcd.print(msg);
 }
 
-void refreshLCD() {
+void refreshLCD() 
+{
   lcd.clear();
   // passar display message para as globais
   String displayMsg = "";
   //lcdState = 1;
-  switch (lcdState) {
+  switch (lcdState) 
+  {
     // Para poder declarar variavel dentro de um case, é necessário um bloco {}
-    case 0: {
-        
-      } break;
-    case 1:
+    case 0:
       displayMsg = (pumpState) ? F("Irrigacao: ON") : F("Irrigacao: OFF");
       lcdPrint(0, 0, displayMsg);
 
       displayMsg = (exaustingState) ? F("Exaustao: ON") : F("Exaustao: OFF");
       lcdPrint(0, 1, displayMsg);
       break;
-    case 2:
+    case 1:
       //displayMsg = (automaticIrrigation) ? F("Irrigacao A: ON") : F("Irrigacao A: OFF");
       displayMsg = "IP:" + IpAddressToString(WiFi.localIP());
       lcdPrint(0, 0, displayMsg);
@@ -277,9 +305,12 @@ void refreshLCD() {
       //displayMsg = (automaticExausting) ? F("Exaustao A: ON") : F("Exaustao A: OFF");
       lcdPrint(0, 1, displayMsg);
       break;
+    
+    // Adicionar cases que mostram os estados de cada seção
   }
 
-  lcdState = (lcdState == 2) ? 0 : lcdState + 1;
+  // lcdState tem que ser igual igual ao ultimo caso
+  lcdState = (lcdState == 1) ? 0 : lcdState + 1;
 }
 
 String IpAddressToString(const IPAddress& ipAddress)
--- config/tools.js
+++ config/tools.js
@@ -2,7 +2,7 @@ module.exports = {
     timeStringToDatabase: function (hourString) {
         return hourString.substring(0, 2) * 3600 + hourString.substring(3, 5) * 60 + hourString.substring(6,8);
     },
-    databaseTimeToString: function (databaseTime) {
+    databaseTimeToString: function (storedTime) {
         let hours = storedTime / 3600;  // needs to be an integer division
         let leaves = storedTime - hours * 3600;
         let minutes = leaves / 60;
